30e5747c47c481551f3118952ead850cfc375166
webkesh <keshavtomar999@gmail.com>
2020-06-20 13:32:52 +0530

    Updates

---
+++ precache-manifest.05975472c411441112ecdfe7573048db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010708d172cf3193e816a24a3e58a963",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "4ddaf1b2fe175bee8589",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "083660d8cbb931e44150",
+    "url": "/facedetection/static/css/main.2ae78eaa.chunk.css"
+  },
+  {
+    "revision": "4ddaf1b2fe175bee8589",
+    "url": "/facedetection/static/js/2.fd9b5e00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facedetection/static/js/2.fd9b5e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083660d8cbb931e44150",
+    "url": "/facedetection/static/js/main.c5b671f2.chunk.js"
+  },
+  {
+    "revision": "95897e3ea03299122fc8",
+    "url": "/facedetection/static/js/runtime-main.ca11d044.js"
+  }
+]);
